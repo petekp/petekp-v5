55a3233a9fce14a54b51f79434bed7fe8d888df6
petekp <pete.petrash@gmail.com>
2016-05-29 19:18:16 -0700

    circle anim test

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,7 +1,67 @@
-var canvas = document.getElementById('Canvas'),
-    context = canvas.getContext('2d'),
-    windowWidth = window.outerWidth,
-    windowHeight = window.outerHeight;
+var mainCanvas = document.getElementById("Canvas");
+var mainContext = mainCanvas.getContext('2d');
 
-context.fillStyle = '#444';
-context.fillRect(0, 0, windowWidth, windowHeight);
+var circles = [];
+
+function Circle(radius, speed, width, xPos, yPos) {
+    this.radius = radius;
+    this.speed = speed;
+    this.width = width;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.opacity = 0.02 + Math.random() * 0.5;
+
+    this.counter = 0;
+
+    var signHelper = Math.floor(Math.random() * 2);
+
+    if (signHelper == 1) {
+        this.sign = -1;
+    } else {
+        this.sign = 1;
+    }
+}
+
+Circle.prototype.update = function () {
+    this.counter += this.sign * this.speed;
+
+    mainContext.beginPath();
+    mainContext.arc(this.xPos + Math.cos(this.counter / 100) * this.radius,
+                    this.yPos + Math.sin(this.counter / 100) * this.radius,
+                    this.width,
+                    0,
+                    Math.PI * 2,
+                    false);
+
+    mainContext.closePath();
+
+    mainContext.fillStyle = 'rgba(40, 0, 283,' + this.opacity + ')';
+    mainContext.fill();
+};
+
+function setupCircles() {
+    for (var i = 0; i < 100; i++) {
+        var randomX = Math.round(-200 + Math.random() * 700);
+        var randomY = Math.round(-200 + Math.random() * 700);
+        var speed = 0.2 + Math.random() * 3;
+        var size = 1 + Math.random() * 30;
+        var radius = 1 + Math.random() * 2;
+
+        var circle = new Circle(radius, speed, size, randomX, randomY);
+        circles.push(circle);
+    }
+    drawAndUpdate();
+}
+setupCircles();
+
+function drawAndUpdate() {
+    mainContext.clearRect(0, 0, 500, 500);
+
+    for (var i = 0; i < circles.length; i++) {
+
+        var myCircle = circles[i];
+        myCircle.update();
+    }
+
+    requestAnimationFrame(drawAndUpdate);
+}
